0196ea9672b9fd8eba0be4761374b2aff0244161
박진우 <jintakk6531@gmail.com>
2021-01-26 14:21:29 +0900

    이미지 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+
   @override
   void initState() {
     super.initState();
